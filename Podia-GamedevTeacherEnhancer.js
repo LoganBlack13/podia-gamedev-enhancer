c4443857419e06d8f71417c35185e1b1ff8ec9af
Logan Black <gaming.loganblack@gmail.com>
2024-03-20 22:24:34 +0100

    added video duration and automatic go to next lesson

--- Podia-GamedevTeacherEnhancer.js
+++ Podia-GamedevTeacherEnhancer.js
@@ -5,13 +5,14 @@
 // ==/UserScript==
 
 let currentUrl = window.location.href;
-const completionButton = document.querySelector('#completion-button');
+let videoDuration = 0;
 
 function EnhancePodia(){
     // Select <section> with parent ID "customer-sidebar"
     const sectionElement = document.querySelector('#customer-sidebar section');
     const h1Element = document.querySelector('#lesson-header h1');
-    
+    const completionButton = document.querySelector('#completion-button');
+
     //Code for search bar
     if (sectionElement) {
         const divSearch = document.createElement('div');
@@ -20,19 +21,19 @@ function EnhancePodia(){
         searchInput.type = 'text';
         searchInput.placeholder = 'Rechercher...';
         searchInput.classList.add('search-input');
-        
+
         divSearch.appendChild(searchInput);
         sectionElement.parentNode.appendChild(divSearch);
-        
+
         // Select all <li> el from <ol> except first
         const listItems = document.querySelectorAll('#customer-sidebar ol li:not(:first-child) ol li');
-        
+
         searchInput.addEventListener('input', function() {
             const searchText = this.value.toLowerCase();
-            
+
             listItems.forEach(function(item) {
                 const text = item.textContent.toLowerCase();
-                
+
                 if (text.includes(searchText)) {
                     item.style.display = '';
                 } else {
@@ -43,29 +44,133 @@ function EnhancePodia(){
     } else {
         console.log('El not found sidebar section');
     }
-    
+
     //Scroll to the current chapter
     if (h1Element) {
         const h1Text = h1Element.textContent.trim();
         const sixFirstCharacters = h1Text.substring(0, 6);
         const sidebarLinks = document.querySelectorAll('#customer-sidebar a');
-        
+
         sidebarLinks.forEach(link => {
             if (link.textContent.trim().substring(0, 6) === sixFirstCharacters) {
                 link.classList.add('highlighted-link');
                 link.scrollIntoView({ behavior: 'smooth', block: 'center'});
             }
-        });  
+        });
     } else {
         console.log("El not found Chapter title");
     }
+
+    //code for completion button click
+    if(completionButton){
+        const link = completionButton.querySelector('a[data-method="post"]');
+        //comment this block to disabled automatic go to next lesson when clicking on completion button
+        //begin block to comment //
+        if(link){
+            link.addEventListener('click', function (e) {
+                goToNextLesson();
+            });
+        }else{
+            listenToastNotification();
+        }
+        //end block to comment //
+
+        //uncomment next line when disabling automatic go to next lesson
+        //listenToastNotification();
+    }else {
+        console.log("El not found : completion button");
+    }
+
+    //VideoPlayer -- Beta
+    //comment next line if script became unstable
+    playerSetup(videoDuration);
 }
 
-//code for completion button click
-if(completionButton){
+// as podia is react app, this code help to detect if url has changed, and refresh the whole script
+setInterval(function() {
+    const newUrl = window.location.href;
+    if (newUrl !== currentUrl) {
+        currentUrl = newUrl;
+        console.log('refresh enhance podia');
+        EnhancePodia();
+    }
+}, 2000);
+
+//First load
+EnhancePodia();
+
+//videoPlayer script - Beta
+function playerSetup(videoDuration)
+{
+    // Get the HTML element with class "wistia_video_foam_dummy"
+    const playerContainer = document.querySelector('.wistia_video_foam_dummy');
+    if(playerContainer === null){
+        return;
+    }
+    // Extract the value of the data-source-container-id attribute
+    const attributeValue = playerContainer.getAttribute('data-source-container-id');
+    // Use regular expression to extract the string
+    if(attributeValue === null){
+        return;
+    }
+    const match = attributeValue.match(/wistia-(\w+)-\d+/);
+    // Check if there's a match and get the extracted string
+    const extractedString = match ? match[1] : null;
+
+    window._wq = window._wq || [];
+    if(extractedString !== null){
+        _wq.push({ id: extractedString, onReady: function(video) {
+            videoDuration = video.duration();
+            console.log(videoDuration);
+            video.videoQuality(1080);
+            // add videoDuration
+            const roundButton = document.querySelector('#display-video-duration');
+            if(roundButton == null){
+                const roundButton = document.createElement('button');
+                roundButton.setAttribute('id','display-video-duration');
+                //roundButton.style.position = 'fixed';
+                //roundButton.style.bottom = '18px'; // Adjust bottom position as needed
+                //roundButton.style.right = '100px'; // Adjust right position as needed
+                roundButton.style.width = '100px';
+                roundButton.style.height = '44px';
+                roundButton.style.borderRadius = '10px';
+                roundButton.style.backgroundColor = '#007bff'; // Change color as needed
+                roundButton.style.color = '#fff';
+                roundButton.style.border = 'none';
+                roundButton.style.cursor = 'default';
+                roundButton.style.display = 'flex';
+                roundButton.style.justifyContent = 'center';
+                roundButton.style.alignItems = 'center';
+                roundButton.style.boxShadow = '0px 0px 10px rgba(0, 0, 0, 0.3)';
+                //roundButton.style.zIndex = '60';
+                roundButton.textContent = Math.floor((videoDuration % 3600) / 60) + 'min ' + Math.floor(videoDuration % 60);
+
+                // Append the button to the navigation section
+                const navElement = document.querySelector('#lesson-navigation nav');
+                navElement.insertBefore(roundButton, navElement.firstChild);
+            }else{
+                roundButton.textContent = Math.floor((videoDuration % 3600) / 60) + 'min ' + Math.floor(videoDuration % 60);
+            }
+        }});
+    }
+}
+
+//automatic go to next lesson
+function goToNextLesson(){
+    const link = document.querySelector('a[rel="next"][aria-label="Continuer"]');
+    if (link) {
+        link.click();
+    } else {
+        console.log('Link not found');
+    }
+}
+
+// listen if a new notification appear on website and refresh the whole script if so.
+function listenToastNotification()
+{
     const targetNode = document.getElementById('toasts-container');
     const config = { childList: true, subtree: true };
-    
+
     const callback = function(mutationsList, observer) {
         for (let mutation of mutationsList) {
             if (mutation.type === 'childList') {
@@ -81,19 +186,6 @@ if(completionButton){
     if (targetNode) {
         observer.observe(targetNode, config);
     } else {
-        console.error('Toast container not found');
+        console.log('Toast container not found');
     }
-}else {
-    console.log("El not found : completion button");
 }
-
-setInterval(function() {
-    const newUrl = window.location.href;
-    if (newUrl !== currentUrl) {
-        currentUrl = newUrl;
-        EnhancePodia();
-    }
-}, 2000);
-
-//First load
-EnhancePodia();
--- README.md
+++ README.md
@@ -1,14 +1,25 @@
-# podia-gamedev-enhancer
+# podia-gamedev-enhancer - made with ❤️
 This project aim to enhance experience with Podia learning website.
 It includes :
 - A visual rework of the page
 - a script to follow on wich lesson you're currently
 - a search field to help you find suject in the long list of video
+## ⚠️ Updates ⚠️
+- added a Tampermonkey version that is more stable than greasemonkey
+   - this version is bundled in a zip archive that you can import directly in tampermonkey, but you also can unzip it and copy/paste code.
+- try to add video duration, but still buggy, regarding time to Wistia to load.
+   - If I had enough time, I'll try to fix it, any help is welcome
+- added an automatic go to next lesson on validate, see source code comments to disable it if you prefere
+- updated Style to be able to add video duration just before navigation buttons
+   - if you deactivate videoplayer - beta script you can avoid to update style.
 ## Tools required
 to avoid making extension for each browser, I decided to use the followinf tools :
 - Stylus, a browser extension to rewrite style
    - [Firefox](https://addons.mozilla.org/fr/firefox/addon/styl-us/)
    - [Chrome](https://chromewebstore.google.com/detail/stylus/clngdbkpkpeebahjckkjfobafhncgmne)
+- Tampermonkey, an extension to execute javascript on any website (Recommended)
+   - [Firefox](https://addons.mozilla.org/fr/firefox/addon/tampermonkey/)
+   - [Chrome](https://chromewebstore.google.com/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo)
 - Greasemonkey, an extension to execute javascript on any website
    - [Firefox](https://addons.mozilla.org/fr/firefox/addon/greasemonkey/)
    - For other web browser you can use Violentmonkey that seems compatible, but not tested.
@@ -38,3 +49,5 @@ You can use each Script separately, regarding you only need a re styled version 
 ![Search](Screenshots/Filter-search-in-chapter.jpg)
 ### Autoscroll & Highlight on chapter
 ![Autoscroll](Screenshots/Automatic-scroll.gif)
+### Video duration
+![Duration](Screenshots/video-duration.jpg)
